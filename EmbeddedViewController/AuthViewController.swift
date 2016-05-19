a6af6ee5ac94b7c5d73aad21402294f87068b314
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-05-19 15:59:27 -0300

    Initial version. Autolayout seems working fine.

---
+++ EmbeddedViewController/AuthViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AuthViewController.swift
+//  EmbeddedViewController
+//
+//  Created by José Inácio Athayde Ferrarini on 19/05/16.
+//  Copyright © 2016 com.inacioferrarini. All rights reserved.
+//
+
+import UIKit
+
+public class AuthViewController: UIViewController {
+
+    @IBAction func performAuth() {
+        print ("Performing Auth")
+    }
+
+}
---
+++ EmbeddedViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmbeddedViewController
+//
+//  Created by José Inácio Athayde Ferrarini on 19/05/16.
+//  Copyright © 2016 com.inacioferrarini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
